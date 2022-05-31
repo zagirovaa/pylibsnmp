8722d980810e54f0359d6214f8bbdc1a4b8481de
Abdul Zagirov <zagirovaa@netcon.pro>
2022-05-31 16:29:06 +0300

    Updated documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'pylibsnmp'
+copyright = '2022, NETCON'
+author = 'Abdul Zagirov'
 
 release = '0.1'
 version = '0.1.0'
