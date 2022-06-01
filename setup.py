c6d054969b78ad6949b4634ff778fc00a8263f44
Abdul Zagirov <zagirovaa@netcon.pro>
2022-06-01 11:46:21 +0300

    Changed docs conf.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="pylibsnmp",
+    version="0.1",
+    description="Library for working with network devices via snmp.",
+    long_description="",
+    author="Abdul Zagirov",
+    author_email="zagirovaa@netcon.pro",
+    license="GNU GPLv3",
+    packages=["pylibsnmp"],
+    zip_safe=False,
+    install_requires=[]
+)
