815189aa7f8e2a88cc02cbc40418783143923a77
Abdul Zagirov <zagirovaa@netcon.pro>
2022-06-06 11:51:16 +0300

    Changed sphinx theme.

--- docs/conf.py
+++ docs/conf.py
@@ -34,8 +34,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
